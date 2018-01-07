96b136cf3b904a2f09c08853c8a7d5fa5f09f787
Sandor Balo <sandorbalo3@gmail.com>
2018-01-07 01:38:29 -0500

    created basic implementation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SudokuBoard from './components/SudokuBoard';
 
 class App extends Component {
+  constructor() {
+    super();
+    // console.log(this);
+    // this._value = "nothing";
+    // this.state = { value: "nothing" };
+    this.state = {
+      startSudoku:
+        "3.542.81.4879.15.6.29.5637485.793.416132.8957.74.6528.2413.9.655.867.192.965124.8"
+        
+    }
+    this.state.boardInstance = new SudokuBoard({startSudoku:this.state.startSudoku});
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.boardInstance.render()}
       </div>
-    );
+    )
   }
 }
 
